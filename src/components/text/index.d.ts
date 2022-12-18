a8bc42818ffa5dbb151ec346d2678c8541bc3b78
Weverson-Luan <wltech2022@gmail.com>
2022-12-17 23:11:50 -0300

    chore :wrench: Feito configuração para usar o styled-components.

---
+++ src/components/text/index.d.ts
@@ -0,0 +1,35 @@
+/**
+ * IMPORTS
+ */
+
+import {TextProps} from 'react-native';
+
+interface IFontsNames {
+  OpenSans;
+  'OpenSans-Italic';
+}
+interface ITextProps extends TextProps {
+  text?: string;
+  width?: number;
+  size?: number;
+  color?: string;
+  align?: 'center' | 'left' | 'right';
+  padding?: number;
+  marginBottom?: number;
+  marginTop?: number;
+  marginLeft?: number;
+  marginRight?: number;
+  margin?: number;
+  textDecoration?: 'line-through' | 'underline' | 'none';
+  weight?: '300' | '400' | '500' | '600' | '700';
+  ellipsis?: boolean;
+  fontFamily?: string;
+  letterSpacing?: number;
+  letterHeight?: number;
+  zIndex?: number;
+}
+
+/**
+ * EXPORTS
+ */
+export {ITextProps};
---
+++ src/screens/home-stack/index.tsx
@@ -0,0 +1,37 @@
+/**
+ * IMPORT
+ */
+import React from 'react';
+
+//components
+import {Text} from '../../components/text';
+
+//assets
+
+//typings
+
+//styles
+import {Container} from './styles';
+import {useTheme} from 'styled-components';
+
+const HomeStack = () => {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Text
+        text="Home Stack"
+        fontFamily="Raleway-Bold"
+        color={theme.colors.neutral_100}
+        size={35}
+        letterHeight={35}
+        weight="700"
+        marginTop={60}
+      />
+    </Container>
+  );
+};
+
+/**
+ * EXPORTS
+ */
+export {HomeStack};
